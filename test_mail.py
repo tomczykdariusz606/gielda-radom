3225a4c0c0fa37c5b2ab1a55903c9c4eb1226c2e
tomczykdariusz606 <tomczykdariusz606@gmail.com>
2026-01-28 22:43:22 +0100

    Update test_mail.py

--- test_mail.py
+++ test_mail.py
@@ -1,7 +1,22 @@
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USERNAME'] = 'tomczykdariusz606@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ngldaqat nibxzvpy'  # Ten sam co w teście
-app.config['MAIL_DEFAULT_SENDER'] = 'tomczykdariusz606@gmail.com'
+import smtplib
+import ssl
+
+# Dane dla Gmail
+smtp_server = "smtp.gmail.com"
+port = 465  # Dla SSL
+sender_email = "tomczykdariusz606@gmail.com"
+password = "ngldaqatnibxzvpy" # Tutaj wklej kod od Google
+
+message = """Subject: Test Gielda Radom
+
+To jest wiadomosc testowa wyslana prosto z serwera Ubuntu."""
+
+context = ssl.create_default_context()
+
+try:
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, sender_email, message)
+    print("Sukces! Mail zostal wyslany pomyslnie.")
+except Exception as e:
+    print(f"Blad: {e}")
