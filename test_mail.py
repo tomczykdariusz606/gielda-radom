59e77cf89463fff98b15478f8331fd0e89f71847
tomczykdariusz606 <tomczykdariusz606@gmail.com>
2026-01-28 22:26:15 +0100

    Update test_mail.py

--- test_mail.py
+++ test_mail.py
@@ -1,7 +1,7 @@
 import smtplib
 try:
     server = smtplib.SMTP_SSL('poczta.o2.pl', 465)
-    server.login('dariusztom@go2.pl', '5WZR5F66GGH6WAEN’)
+    server.login('dariusztom@go2.pl', '5WZR5F66GGH6WAEN')
     server.sendmail('dariusztom@go2.pl', 'dariusztom@go2.pl', 'Temat: Test\n\nTo jest testowa wiadomosc.')
     server.quit()
     print("Mail wysłany pomyślnie!")
