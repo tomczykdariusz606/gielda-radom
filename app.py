bffd353dff1f1d4559d77dda39bcb51c868e1051
tomczykdariusz606 <tomczykdariusz606@gmail.com>
2026-02-02 20:21:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,6 +89,7 @@ class Car(db.Model):
     model = db.Column(db.String(50), nullable=False)
     rok = db.Column(db.Integer, nullable=False)
     cena = db.Column(db.Float, nullable=False)
+    przebieg = db.Column(db.Integer, default=0)
     opis = db.Column(db.Text, nullable=False)
     telefon = db.Column(db.String(20), nullable=False)
     img = db.Column(db.String(200), nullable=False) 
