8907d115af6be750fcc99577a8d74be49eb80048
tomczykdariusz606 <tomczykdariusz606@gmail.com>
2026-02-05 06:27:44 +0100

    Update latka.py

--- latka.py
+++ latka.py
@@ -1,13 +1,6 @@
-from app import app, db
-from sqlalchemy import text
-
-def napraw_dane():
-    with app.app_context():
-        # Ustawiamy 0 tam, gdzie są puste wartości, aby formatowanie w HTML nie wywalało błędu
-        db.session.execute(text('UPDATE car SET przebieg = 0 WHERE przebieg IS NULL'))
-        db.session.execute(text('UPDATE car SET cena = 0 WHERE cena IS NULL'))
-        db.session.commit()
-        print("Sukces: Puste wartości zostały zastąpione zerami.")
-
-if __name__ == "__main__":
-    napraw_dane()
+import sqlite3
+conn = sqlite3.connect('instance/gielda.db')
+cursor = conn.cursor()
+cursor.execute("SELECT id, username FROM user")
+print(cursor.fetchall())
+conn.close()
